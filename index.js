42946a6ef0fd132d4f95be90dd2771072a91b264
Shun Iwashita <iwashunabc519@gmail.com>
2021-11-03 14:49:55 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 
-// Factory
-function createCircle(radius) {
-  
+////  Factory Function  ////
+function createCircle(radius, x = 1, y = 1) {
+   return {
+      radius,
+      position: {
+         x,
+         y
+      },
+      draw: () => {
+         console.log('draw');
+      }
+   }
 }
+
+const circle = createCircle(12);
+circle.draw();
+// ################### //
+
+
+////  Constructor Function ////
+function Circle(radius, x = 1, y = 1){
+   this.radius = radius;
+   this.position = {
+      x,
+      y
+   }
+   this.draw = () => {
+      console.log('draw');
+   }
+}
+
+// require 'new', if you didn't type 'new', it will defind 'this' as 'window' object.
+const circle1 = new Circle(4);
+circle1.draw();
+// ################### //
+
+
+
+
+
+
+
